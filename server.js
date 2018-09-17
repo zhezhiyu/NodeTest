7c3b80671b3f04eb8b968e5d9e676eb5bd913db5
zhezhiyu <39079894+zhezhiyu@users.noreply.github.com>
2018-09-17 11:37:42 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,18 @@ const server = http.createServer((req, res) => {
   // 解析请求，包括文件名
 var pathname = url.parse(req.url).pathname;
 console.log("Request for " + pathname + " received.");
-
+fs.readFile(pathi.name.substr(1), function (err, data) {
+  
+  if (err) {
+    console.log(err);
+    
+    response.writeHead(404, {'Content-Type': 'text/html'});
+  }else{
+    response.writeHead(200, {'Content-Type':'text/html'});
+    
+  }
+  response.end('♪(^∇^*)');
+});
 
 });
 server.listen(port, hostname, () => {

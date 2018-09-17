43ac46311ac62caa4d1fc3dea77fd706ee1e3bcd
zhezhiyu <39079894+zhezhiyu@users.noreply.github.com>
2018-09-17 11:06:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,23 @@
-
-	var http = require('http');
-var optfile = require("fs");
-http.createServer(function(request,response){
-    response.writeHead(200,{'Content-Type' : 'text/html; charset=UTF-8'});
-    if(request.url != '/favicon.ico'){
-        function recall(data){  //等待readfile执行完之后回调
-            response.write(data);
-            response.end('ok');
-        }
-        optfile.readfile('./index.html',recall);//传入闭包函数
-        console.log("主程序结束");
-    }   
-}).listen(3000);
-var fs = require("fs");
-
-module.exports = {
-
-    readfileSync : function(path){//同步读取
-        var data = fs.readFileSync(path,'utf-8');
-        console.log(data);
-        console.log("同步方法执行完毕");
-    },
-
-    readfile : function(path,recall){//异步执行
-        fs.readFile(path,function(err,data){
-            if(err){
-                console.log(err);
-            }else{
-                recall(data);   //回调recall函数，它是闭包函数，它会存储原来的response对象
-                console.log(data.toString());
-            }
-        });
-        console.log("异步方法执行完毕");
-
-    }
-}
+var http = require("http")
+var fs = require("fs")
+ 
+http.createServer(function(req,res){
+	var path = req.url;	
+	console.log("path1: "+path)
+	if(path == "/"){
+		path = "/index.html";
+	}
+	sendFile(res,path);
+}).listen(3000)
+ 
+function sendFile(res,path){
+	var path = process.cwd()+path;
+	fs.readFile(path,function(err,stdout,stderr){
+		if(!err){
+			var data = stdout;
+			var type = path.substr(path.lastIndexOf(".")+1,path.length)
+			res.writeHead(200,{'Content-type':"text/"+type});	//在这里设置文件类型，告诉浏览器解析方式
+			res.write(data);
+		}
+		res.end();
+	})

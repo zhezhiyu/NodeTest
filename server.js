fd57711cef6325c2498f3b9534d091076dea3a26
zhezhiyu <39079894+zhezhiyu@users.noreply.github.com>
2018-09-17 11:36:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,18 +8,7 @@ const server = http.createServer((req, res) => {
 var pathname = url.parse(req.url).pathname;
 console.log("Request for " + pathname + " received.");
 
-fs.readFile(pathi.name.substr(1)), function (err, data) {
-  
-  if (err) {
-    console.log(err);
-    
-    response.writeHead(404, {'Content-Type': 'text/html'});
-  }else{
-    response.writeHead(200, {'Content-Type':'text/html'});
-    
-  }
-  response.end('♪(^∇^*)');
-});
+
 });
 server.listen(port, hostname, () => {
 console.log(`Server running at http://${hostname}:${port}/`);

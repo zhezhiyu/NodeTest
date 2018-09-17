0bc51b19928c44239c9458930c4fe67ed4fdaf94
zhezhiyu <39079894+zhezhiyu@users.noreply.github.com>
2018-09-17 10:42:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,108 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+
+	/**
+		
+	1.使用 HTTP 服务器与客户端交互，需要 require('http')。
+		声明http协议
+	*/
+	var http = require('http');
+	
+	
+	// 声明文件操作系统对象
+	var fs = require('fs');
+	/**
+	2.获取服务器对象
+		1.通过 http.createServer([requestListener]) 创建一个服务
  
+		requestListener <Function>
+		返回: <http.Server>
+		返回一个新建的 http.Server 实例。
+		对于服务端来说，主要做三件事：
+		1.接受客户端发出的请求。
+		2.处理客户端发来的请求。
+		3.向客户端发送响应。
+	*/
+	
+	var server = http.createServer();
  
-// 创建服务器
-http.createServer( function (request, response) {  
-   // 解析请求，包括文件名
-   var pathname = url.parse(request.url).pathname;
-   
-   // 输出请求的文件名
-   console.log("Request for " + pathname + " received.");
-   
-   // 从文件系统中读取请求的文件内容
-   fs.readFile(pathname.substr(1), function (err, data) {
-      if (err) {
-         console.log(err);
-         // HTTP 状态码: 404 : NOT FOUND
-         // Content Type: text/plain
-         response.writeHead(404, {'Content-Type': 'text/html'});
-      }else{             
-         // HTTP 状态码: 200 : OK
-         // Content Type: text/plain
-         response.writeHead(200, {'Content-Type': 'text/html'});    
-         
-         // 响应文件内容
-         response.write(data.toString());        
-      }
-      //  发送响应数据
-      response.end();
-   });   
-}).listen(8080);
+	/**
+	3.声明端口号，开启服务。
+		server.listen([port][, host][, backlog][, callback])
  
-// 控制台会输出以下信息
-console.log('Server running at http://127.0.0.1:8080/');
+		port <number> ：端口号
+		host <string> ：主机ip
+		backlog <number> server.listen() 函数的通用参数
+		callback <Function> server.listen() 函数的通用参数
+		Returns: <net.Server>
+		启动一个TCP服务监听输入的port和host。
+ 
+		如果port省略或是0，系统会随意分配一个在'listening'事件触发后能被server.address().port检索的无用端口。
+ 
+		如果host省略，如果IPv6可用，服务器将会接收基于unspecified IPv6 address (::)的连接，否则接收基于unspecified IPv4 address (0.0.0.0)的连接
+	
+	*/
+	server.listen(3011,0.0.0.0, function(){
+		 console.log('服务器正在端口号：3011上运行......');
+	})
+	
+	
+	/**
+	4.给server 实例对象添加request请求事件，该请求事件是所有请求的入口。
+		任何请求都会触发改事件，然后执行事件对应的处理函数。
+	
+		server.on('request',function(){
+			 console.log('收到客户端发出的请求.......');
+		});
+	*/
+ 
+	
+	/**
+	5.设置请求处理函数。
+		请求回调处理函数需要接收两个参数。
+		request ：request是一个请求对象，可以拿到当前浏览器请求的一些信息。
+			eg：请求路径，请求方法等
+		response： response是一个响应对象，可以用来给请求发送响应。
+	
+	*/
+	server.on('request',function(request,response){
+		
+		var url = request.url;
+		if(url ==='/'){
+			//response.writeHead(响应状态码，响应头对象): 发送一个响应头给请求。
+			response.writeHead(200,{'Content-Type':'text/html'})
+			// 如果url=‘/’ ,读取指定文件下的html文件，渲染到页面。
+			fs.readFile('./index.html','utf-8',function(err,data){
+				if(err){
+					throw err ;
+				}
+				response.end(data);
+			});
+		
+		}else if(url === '/login'){
+			response.writeHead(200,{'Content-Type':'text/html'});
+			// 如果url=‘/’ ,读取指定文件下的html文件，渲染到页面。
+			fs.readFile('./login.html','utf-8',function(err,data){
+				if(err){
+					throw err ;
+				}
+				response.end(data);
+			});
+		}else if(url === '/index'){
+			response.writeHead(200,{'Content-Type':'text/html'});
+			// 如果url=‘/’ ,读取指定文件下的html文件，渲染到页面。
+			fs.readFile('./index.html','utf-8',function(err,data){
+				if(err){
+					throw err ;
+				}
+				response.end(data);
+			});
+		}else{
+			response.writeHead(200,{'Content-Type':'text/html'});
+			// 如果url=‘/’ ,读取指定文件下的html文件，渲染到页面。
+			fs.readFile('./notFount.html','utf-8',function(err,data){
+				if(err){
+					throw err ;
+				}
+				response.end(data);
+			});
+		}
+		
